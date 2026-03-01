8f11cc607eabadfd0c87996dccc23177d053d493
Israel Oyedele <israeloyedele01@gmail.com>
2026-02-28 23:48:04 -0500

    Refined CSS

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export function Footer() {
             whileHover={{ scale: 1.05, transition: { duration: 0.2 }, y: -2 }}
             whileTap={{ scale: 0.9, transition: { duration: 0.2 }}}
             className="footer">
-             <a href="https://israeloyedele.com" target="_blank">&copy; Israel Oyedele 2026</a>
+             <a href="https://israeloyedele.com" target="_blank">&copy; 2026 Israel Oyedele</a>
         </motion.div>
     )
 }
