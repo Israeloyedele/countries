c38506bed5d7cdeae8378a7ae09800365641845f
Israeloyedele <131530863+Israeloyedele@users.noreply.github.com>
2026-03-01 06:02:47 +0100

    Reduce animation

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,7 +11,7 @@ export function Card(props) {
     return (
         <motion.li
             ref={ref}
-            initial={{ opacity: 0, y: 100, scale: .6 }}
+            initial={{ opacity: 0, y: 50, scale: .6 }}
             animate={ isInView && { opacity: 1, y: 0, scale: 1, transition: { duration: 1, type: "spring" } }}
             whileHover={{ scale: 1.05, transition: { duration: 0.2 }, y: -2 }}
             transition={{ duration: 0.3 }}
