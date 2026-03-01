b260e3e5917ef121beecafd9af1fe305268ec572
Israel Oyedele <israeloyedele01@gmail.com>
2026-03-01 04:37:49 -0500

    Refined CSS

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -11,8 +11,8 @@ export function Card(props) {
     return (
         <motion.li
             ref={ref}
-            initial={{ opacity: 0, y: 50, scale: .6 }}
-            animate={ isInView && { opacity: 1, y: 0, scale: 1, transition: { duration: 1, type: "spring" } }}
+            initial={{ opacity: 0, scale: .6 }}
+            animate={ isInView && { opacity: 1, scale: 1, transition: { duration: 1, type: "spring" } }}
             whileHover={{ scale: 1.05, transition: { duration: 0.2 }, y: -2 }}
             transition={{ duration: 0.3 }}
 
