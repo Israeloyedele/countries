411fa37eaffcfeed1714f43d04448146728a5d73
Israel Oyedele <israeloyedele01@gmail.com>
2026-03-01 05:51:20 -0500

    Add Sorting

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -2,11 +2,12 @@ import { Card } from "./Card.jsx";
 
 export function Countries(props) {
     const { countries } = props;
+    const sortedCountries = countries.sort((a, b) => a.name?.common.localeCompare(b.name?.common));
 
     return (
         <ul>
-            {countries.length > 0 ?
-                countries?.map(country => <Card key={country.name.common} country={country}/>)
+            {sortedCountries.length > 0 ?
+                sortedCountries?.map(country => <Card key={country.name.common} country={country}/>)
                 : <p>No Countries match your search.</p>}
         </ul>
     )
